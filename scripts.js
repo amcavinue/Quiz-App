40bb7424046f35d7042db103a4e9cb0305016246
amcavinue <aaronmcavinue@gmail.com>
2016-10-15 05:11:59 -0400

    More page logic

--- scripts.js
+++ scripts.js
@@ -63,7 +63,6 @@ $(function() {
 		wrong: 0
 	};
 
-	// Watch start quiz button.
 	$('#js-start-quiz').click(function(e){
 		e.preventDefault();
 		getQuestion(state, questions);
@@ -75,7 +74,8 @@ $(function() {
 function getQuestion(state, questions) {
 	var randIndex = Math.floor(Math.random() * (questions.length)),
 		question = questions[randIndex],
-		$jsQuestions = $('#js-questions');
+		$jsQuestions = $('#js-questions'),
+		answer = questions[randIndex].correct;
 
 	questions.splice(randIndex, 1);
 	$jsQuestions.append(renderQuestion(state, question));
@@ -84,18 +84,50 @@ function getQuestion(state, questions) {
 		// Turn off the listener so each question is only answered once.
 		$('.js-answers').off('click');
 
+		$(this).addClass('checked');
+
+		if ($(this).data('question') === answer) {
+			state.correct++;
+		} else {
+			state.wrong++;
+		}
+
+		updateScore(state);
+
 		state.questionNum++;
 
 		if (state.questionNum < 5) {
 			getQuestion(state, questions);
 		} else {
-			getResults();
+			getResults(state);
 		}
 	});
 }
 
-function getResults() {
+function updateScore(state) {
+	$('.js-correct-num').text(state.correct);
+	$('.js-wrong-num').text(state.wrong);
+}
 
+function getResults(state) {
+	$('.js-content').append(renderResults(state));
+}
+
+
+function reset(state) {
+
+}
+
+function closingStatement(state) {
+	if (state.correct === 5) {
+		return 'You love cats!';
+	} else if (state.correct === 4) {
+		return 'You like cats.'
+	} else if (state.correct === 3) {
+		return 'You\'re alright with cats.';
+	} else {
+		return 'You might be a dog person.'
+	}
 }
 
 function renderQuestion(state, qAndA) {
@@ -105,19 +137,19 @@ function renderQuestion(state, qAndA) {
 				'<h2>' + qAndA.question + '</h2>' +
 				'<ul class="js-answers">' +
 					'<li>' +
-						'<span class="checkbox"></span>' +
+						'<span class="checkbox" data-question="a"></span>' +
 						'a.' + qAndA.a +
 					'</li>' +
 					'<li>' +
-						'<span class="checkbox"></span>' +
+						'<span class="checkbox" data-question="b"></span>' +
 						'b.' + qAndA.b +
 					'</li>' +
 					'<li>' +
-						'<span class="checkbox"></span>' +
+						'<span class="checkbox" data-question="c"></span>' +
 						'c.' + qAndA.c +
 					'</li>' +
 					'<li>' +
-						'<span class="checkbox"></span>' +
+						'<span class="checkbox" data-question="d"></span>' +
 						'd.' + qAndA.d +
 					'</li>' +
 				'</ul>' +
@@ -126,4 +158,15 @@ function renderQuestion(state, qAndA) {
 			'</div>' +
 		'</section>' +
 		'<hr />';
+}
+
+function renderResults(state) {
+	return '<section class="results">' +
+			'<h2>You finished the quiz!</h2>' +
+			'<p class="result-stats">' +
+				'You Got <strong><span class="js-correct-num">' + state.correct + '</span>/5 Correct</strong>' +
+			'</p>' +
+			'<p class="closing-statement">' + closingStatement(state) + '</p>' +
+			'<a href="#" class="replay">PLAY AGAIN</a>' +
+		'</section>';
 }
