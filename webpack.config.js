eafc0c092d3bf9c1c70653250930bc2ab060c6ca
amcavinue <aaronmcavinue@gmail.com>
2016-11-06 14:58:04 -0500

    Add uglify for minifying .js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ var packageData = require('./package.json');
 
 var filename = [packageData.name, packageData.version, 'js'];
 
+var plugins = [];
+
+// If you use the --minify argument then
+// it adds a .min extension, and
+// adds it to the plugins array.
+var minify = process.argv.indexOf('--minify') != -1;
+
+if (minify) {
+    filename.splice(filename.length - 1, 0, 'min');
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
 module.exports = {
     entry: path.resolve(__dirname, packageData.main),
     output: {
@@ -24,5 +36,6 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: plugins
 }
